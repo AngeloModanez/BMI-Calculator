5b26234467f13d50541a0c52f0dc0cec79edea32
Bananadev3 <angelomodanez30@gmail.com>
2024-03-08 01:39:47 -0300

    CSS and HTML configs

--- src/app/bmi/bmi.component.ts
+++ src/app/bmi/bmi.component.ts
@@ -12,25 +12,24 @@ export class BmiComponent {
   msg: string = '';
 
   calcBmi(bmi: number) {
-    this.height = this.height / 100;
     bmi = this.weight / (this.height * this.height);
 
     if (bmi < 16) {
-      this.msg = 'Severe Thinness ' + bmi.toFixed(1);
+      this.msg = 'BMI = ' + bmi.toFixed(1) + 'kg/m2 (Severe Thinness)';
     } else if (bmi >= 16 && bmi < 17) {
-      this.msg = 'Moderate Thinness ' + bmi.toFixed(1);
+      this.msg = 'BMI = ' + bmi.toFixed(1) + 'kg/m2 (Moderate Thinness)';
     } else if (bmi >= 17 && bmi < 18.5) {
-      this.msg = 'Mild Thinness ' + bmi.toFixed(1);
+      this.msg = 'BMI = ' + bmi.toFixed(1) + 'kg/m2 (Mild Thinness)';
     } else if (bmi >= 18.5 && bmi < 25) {
-      this.msg = 'Normal ' + bmi.toFixed(1);
+      this.msg = 'BMI = ' + bmi.toFixed(1) + ' kg/m2 (Normal)';
     } else if (bmi >= 25 && bmi < 30) {
-      this.msg = 'Overweight ' + bmi.toFixed(1);
+      this.msg = 'BMI = ' + bmi.toFixed(1) + 'kg/m2 (Overweight)';
     } else if (bmi >= 30 && bmi < 35) {
-      this.msg = 'Obese Class I ' + bmi.toFixed(1);
+      this.msg = 'BMI = ' + bmi.toFixed(1) + 'kg/m2 (Obese Class I)';
     } else if (bmi >= 35 && bmi < 40) {
-      this.msg = 'Obese Class II ' + bmi.toFixed(1);
+      this.msg = 'BMI = ' + bmi.toFixed(1) + 'kg/m2 (Obese Class II)';
     } else if (bmi >= 40) {
-      this.msg = 'Obese Class III ' + bmi.toFixed(1);
+      this.msg = 'BMI = ' + bmi.toFixed(1) + 'kg/m2 (Obese Class III)';
     }
   }
 }
