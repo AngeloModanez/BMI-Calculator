bf639f5c6a0e8b9f3ab88d5da30d93f95f6d888c
Bananadev3 <angelomodanez30@gmail.com>
2024-03-07 23:48:46 -0300

    Upgrade calcBmi function

--- src/app/bmi/bmi.component.ts
+++ src/app/bmi/bmi.component.ts
@@ -3,14 +3,34 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-bmi',
   templateUrl: './bmi.component.html',
-  styleUrl: './bmi.component.css'
+  styleUrl: './bmi.component.css',
 })
 export class BmiComponent {
-  weight: any = '';
-  height: any = '';
-  bmi: number = 0;
+  weight: any;
+  height: any;
+  bmi: any;
+  msg: string = '';
 
-  bmicalc() {
-    this.bmi = (this.weight / (this.height * this.height));
+  calcBmi(bmi: number) {
+    this.height = this.height / 100;
+    bmi = this.weight / (this.height * this.height);
+
+    if (bmi < 16) {
+      this.msg = 'Severe Thinness ' + bmi.toFixed(1);
+    } else if (bmi >= 16 && bmi < 17) {
+      this.msg = 'Moderate Thinness ' + bmi.toFixed(1);
+    } else if (bmi >= 17 && bmi < 18.5) {
+      this.msg = 'Mild Thinness ' + bmi.toFixed(1);
+    } else if (bmi >= 18.5 && bmi < 25) {
+      this.msg = 'Normal ' + bmi.toFixed(1);
+    } else if (bmi >= 25 && bmi < 30) {
+      this.msg = 'Overweight ' + bmi.toFixed(1);
+    } else if (bmi >= 30 && bmi < 35) {
+      this.msg = 'Obese Class I ' + bmi.toFixed(1);
+    } else if (bmi >= 35 && bmi < 40) {
+      this.msg = 'Obese Class II ' + bmi.toFixed(1);
+    } else if (bmi >= 40) {
+      this.msg = 'Obese Class III ' + bmi.toFixed(1);
+    }
   }
 }
