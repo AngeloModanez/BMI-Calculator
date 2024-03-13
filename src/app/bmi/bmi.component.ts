3578a803df041650423d11aabd083727960b45be
Bananadev3 <angelomodanez30@gmail.com>
2024-03-13 03:25:55 -0300

    Scale img and Icon, remove Age

--- src/app/bmi/bmi.component.ts
+++ src/app/bmi/bmi.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { inject, TemplateRef } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { clear } from 'console';
 
 @Component({
   selector: 'app-bmi',
@@ -10,63 +11,62 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 export class BmiComponent {
   private modalService = inject(NgbModal);
 
-  age: any = '';
-  weight: any = '';
-  height: any = '';
-  name: any = '';
+  weight: any;
+  height: any;
+  name: string = '';
+  imgPath: string = '';
   bmi: any;
 
-  // error: string = 'ERROR: FILL THE FORM'
-  // openCleanError(content: TemplateRef<any>) {
-  //   if (this.weight == '' && this.height == ''){
-  //     this.modalService.open(content, { backdropClass: 'light-blue-backdrop' });
-  //   } else{
-  //     this.height = '';
-  //     this.weight = '';
-  //   }
-  // }
-
   clean() {
-    this.age = '';
+    this.weight = null;
+    this.height = null;
     this.name = '';
-    this.height = '';
-    this.weight = '';
+    this.imgPath = '';
   }
 
-  calcBmi(bmi: number, content: TemplateRef<any>,) {
+  calcBmi(bmi: number, content: TemplateRef<any>) {
     this.modalService.open(content, { backdropClass: 'light-blue-backdrop' });
-    if (this.name == '' || this.weight == '' || this.height == '')
+    if (this.name == '' || this.weight == null || this.height == null ) {
       this.bmi = 'ERROR: PLEASE FILL THE FORM';
-
-    if (this.height > 5) {
-      this.height = this.height / 100;
+      this.imgPath = '';
     }
+
+    if (this.height > 5) this.height = this.height / 100;
+
     bmi = this.weight / (this.height * this.height);
 
     switch (true) {
       case bmi < 16:
         this.bmi = bmi.toFixed(1) + 'kg/m2 (Severe Thinness)';
+        this.imgPath = '../assets/bmiimg/severethinness.png';
         break;
       case bmi < 17:
         this.bmi = bmi.toFixed(1) + 'kg/m2 (Moderate Thinness)';
+        this.imgPath = '../assets/bmiimg/moderatethinness.png';
         break;
       case bmi < 18.5:
         this.bmi = bmi.toFixed(1) + 'kg/m2 (Mild Thinness)';
+        this.imgPath = '../assets/bmiimg/mildthinness.png';
         break;
       case bmi < 25:
         this.bmi = bmi.toFixed(1) + 'kg/m2 (Nomal)';
+        this.imgPath = '../assets/bmiimg/normal.png';
         break;
       case bmi < 30:
         this.bmi = bmi.toFixed(1) + 'kg/m2 (Overweight)';
+        this.imgPath = '../assets/bmiimg/overweight.png';
         break;
       case bmi < 35:
         this.bmi = bmi.toFixed(1) + 'kg/m2 (Obese Class I)';
+        this.imgPath = '../assets/bmiimg/obeseclassI.png';
         break;
       case bmi < 40:
         this.bmi = bmi.toFixed(1) + 'kg/m2 (Obese Class II)';
+        this.imgPath = '../assets/bmiimg/obeseclassII.png';
         break;
       case bmi >= 40:
         this.bmi = bmi.toFixed(1) + 'kg/m2 (Obese Class III)';
+        this.imgPath = '../assets/bmiimg/obeseclassIII.png';
         break;
     }
   }
